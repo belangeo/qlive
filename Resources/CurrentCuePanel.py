b06a5a5440dffd6decde979cf60e5d7128ba6e11
Tiago Bortoletto Vaz <tiago@debian.org>
2015-08-27 15:36:20 -0400

    Add floating window to show current cue number.

---
+++ Resources/CurrentCuePanel.py
@@ -0,0 +1,28 @@
+# encoding: utf-8
+
+import wx
+import QLiveLib
+
+class CurrentCuePanel(wx.Frame):
+    def __init__(self, parent):
+        style = wx.DEFAULT_FRAME_STYLE & (~wx.CLOSE_BOX) & (~wx.MINIMIZE_BOX)
+        wx.Frame.__init__(self, parent, title="Current Cue Window", style=style, size=(600, 600))
+        self.SetBackgroundColour(wx.BLACK)
+
+        # TODO: set text during the obj creation
+        # TODO: auto adjust font size to the window size
+        str = "0"
+        self.text = wx.StaticText(self, -1, str)
+        self.text.SetForegroundColour((255,255,255))
+        font = wx.Font(450, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        self.text.SetFont(font)
+
+        # Vertical/Horizontal centering
+        sizer_v = wx.BoxSizer(wx.VERTICAL)
+        sizer_h = wx.BoxSizer(wx.HORIZONTAL)
+        sizer_h.Add(self.text, 1, wx.CENTER)
+        sizer_v.Add(sizer_h, 1, wx.CENTER)
+        self.SetSizer(sizer_v)
+
+    def refresh(self, cue):
+        self.text.SetLabel(str(cue))
