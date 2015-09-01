6057dbd039b7594a4a7b94eb6dea305ea5b63a6d
Tiago Bortoletto Vaz <tiago@debian.org>
2015-09-01 17:48:20 -0400

    Improving preferences dialog, adding new variables to be set.

--- Resources/PreferencePanel.py
+++ Resources/PreferencePanel.py
@@ -30,22 +30,31 @@ class AudioPrefsTab(wx.Panel):
 
         # Setting audio driver
         audioDriverDefault = QLiveLib.getVar("audio")
-        audioDriverChoices = AUDIO_DRIVERS
         audioDriverLabel = wx.StaticText(self, -1, "Audio driver:")
         self.audioDriverCB = wx.ComboBox(self, -1, audioDriverDefault, wx.DefaultPosition,
-                         (140, -1), audioDriverChoices, wx.CB_READONLY|wx.TE_PROCESS_ENTER)
+                         (140, 20), AUDIO_DRIVERS, wx.CB_READONLY|wx.TE_PROCESS_ENTER)
         self.audioDriverCB.Bind(wx.EVT_COMBOBOX, self.setAudioDriver, self.audioDriverCB)
 
         # Setting buffer size
         bufferSizeDefault = QLiveLib.getVar("bufferSize")
-        bufferSizeChoices = ['64','128','256','512','1024','2048']
         bufferSizeLabel = wx.StaticText(self, -1, "Buffer size:")
         self.bufferSizeCB = wx.ComboBox(self, -1, bufferSizeDefault, wx.DefaultPosition,
-                         (140, -1), bufferSizeChoices,
-                         wx.CB_READONLY
-                         | wx.TE_PROCESS_ENTER
-                         )
+                         (140, 20), BUFFER_SIZES, wx.CB_READONLY|wx.TE_PROCESS_ENTER)
         self.bufferSizeCB.Bind(wx.EVT_COMBOBOX, self.setBufferSize, self.bufferSizeCB)
+
+        # Setting sampling rate
+        samplingRateDefault = QLiveLib.getVar("sr")
+        samplingRateLabel = wx.StaticText(self, -1, "Sampling rate:")
+        self.samplingRateCB = wx.ComboBox(self, -1, samplingRateDefault, wx.DefaultPosition,
+                         (140, 20), SAMPLE_RATES, wx.CB_READONLY|wx.TE_PROCESS_ENTER)
+        self.samplingRateCB.Bind(wx.EVT_COMBOBOX, self.setSamplingRate, self.samplingRateCB)
+
+        # Setting duplex True/False
+        duplexDefault = QLiveLib.getVar("duplex")
+        duplexLabel = wx.StaticText(self, -1, "Duplex:")
+        self.duplexCB = wx.CheckBox(self, -1, "", style=wx.ALIGN_LEFT)
+        self.duplexCB.SetValue(bool(int(QLiveLib.getVar("duplex"))))
+        self.duplexCB.Bind(wx.EVT_CHECKBOX, self.setDuplex, self.duplexCB)
 
         # Main box
         box = wx.StaticBox(self, -1, "Audio settings")
@@ -54,16 +63,26 @@ class AudioPrefsTab(wx.Panel):
         vsizer = wx.StaticBoxSizer(box, wx.VERTICAL)
         hsizerAudioDriver = wx.BoxSizer(wx.HORIZONTAL)
         hsizerBufferSize  = wx.BoxSizer(wx.HORIZONTAL)
+        hsizerSamplingRate  = wx.BoxSizer(wx.HORIZONTAL)
+        hsizerDuplex  = wx.BoxSizer(wx.HORIZONTAL)
 
-        hsizerAudioDriver.Add(audioDriverLabel, -1, wx.ALL|wx.ALIGN_CENTER, 5)
-        hsizerAudioDriver.Add(self.audioDriverCB, -1, wx.ALL, 5)
+        hsizerAudioDriver.Add(audioDriverLabel, -1, wx.ALL|wx.ALIGN_CENTER, 3)
+        hsizerAudioDriver.Add(self.audioDriverCB, -1, wx.ALL, 3)
 
-        hsizerBufferSize.Add(bufferSizeLabel, -1, wx.ALL|wx.ALIGN_CENTER , 5)
-        hsizerBufferSize.Add(self.bufferSizeCB, -1, wx.ALL, 5)
+        hsizerBufferSize.Add(bufferSizeLabel, -1, wx.ALL|wx.ALIGN_CENTER , 3)
+        hsizerBufferSize.Add(self.bufferSizeCB, -1, wx.ALL, 3)
 
-        vsizer.AddSpacer(10)
+        hsizerSamplingRate.Add(samplingRateLabel, -1, wx.ALL|wx.ALIGN_CENTER , 3)
+        hsizerSamplingRate.Add(self.samplingRateCB, -1, wx.ALL, 3)
+
+        hsizerDuplex.Add(duplexLabel, -1, wx.ALL|wx.ALIGN_CENTER , 3)
+        hsizerDuplex.Add(self.duplexCB, -1, wx.ALL, 3)
+
+        vsizer.AddSpacer(5)
         vsizer.Add(hsizerAudioDriver, 0, wx.ALL|wx.EXPAND, 0)
         vsizer.Add(hsizerBufferSize, 0, wx.ALL|wx.EXPAND, 0)
+        vsizer.Add(hsizerSamplingRate, 0, wx.ALL|wx.EXPAND, 0)
+        vsizer.Add(hsizerDuplex, 0, wx.ALL|wx.EXPAND, 0)
 
         border = wx.BoxSizer()
         border.Add(vsizer, -1, wx.EXPAND|wx.ALL, 5)
@@ -75,16 +94,27 @@ class AudioPrefsTab(wx.Panel):
     def setBufferSize(self, evt):
         QLiveLib.setVar("bufferSize", evt.GetString())
 
+    def setSamplingRate(self, evt):
+        QLiveLib.setVar("sr", evt.GetString())
+
+    def setDuplex(self, state):
+        if state.GetEventObject().GetValue() == True:
+            QLiveLib.setVar("duplex", "1")
+        else:
+            QLiveLib.setVar("duplex", "0")
+
 #TODO: needs better var names
 class PreferenceFrame(wx.Dialog):
     def __init__(self, parent):
         style = wx.DEFAULT_FRAME_STYLE | wx.FRAME_FLOAT_ON_PARENT
-        wx.Dialog.__init__(self, parent, title="QLive Preferences", style=style, size=(300, 250))
+        wx.Dialog.__init__(self, parent, title="QLive Preferences", style=style, size=(350, 230))
         self.parent = parent
 
         # Create a panel and notebook (tabs holder)
         p = wx.Panel(self)
         nb = wx.Notebook(p)
+        closeButton = wx.Button(p, -1, size=(50,-1), label="Close")
+        closeButton.Bind(wx.EVT_BUTTON, self.onClose)
 
         # Create the tab windows
         tab1 = GeneralPrefsTab(nb)
@@ -96,5 +126,10 @@ class PreferenceFrame(wx.Dialog):
 
         # Set noteboook in a sizer to create the layout
         sizer = wx.BoxSizer(wx.VERTICAL)
-        sizer.Add(nb, 1, wx.EXPAND)
+        sizer.Add(nb, 0, wx.EXPAND)
+        sizer.Add(closeButton, 0, wx.ALL|wx.ALIGN_RIGHT, 5)
         p.SetSizer(sizer)
+
+    def onClose(self, evt):
+        self.Destroy()
+
