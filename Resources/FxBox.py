0c1944f06ec731f4f4a085402e746eca52a6cb30
Olivier Belanger <belangeo@gmail.com>
2015-09-22 08:13:59 -0400

    Fixed FxBox.getCurrentValues when the cue is None.

--- Resources/FxBox.py
+++ Resources/FxBox.py
@@ -166,13 +166,15 @@ class BaseFxBox(object):
     def getCurrentValues(self):
         if self.cues:
             if self.currentCue in self.cues:
-                return self.cues[self.currentCue]["values"]
+		if self.cues[self.currentCue] is not None:
+		    return self.cues[self.currentCue]["values"]
         return None
 
     def getCurrentInterps(self):
         if self.cues:
             if self.currentCue in self.cues:
-                return self.cues[self.currentCue]["interps"]
+		if self.cues[self.currentCue] is not None:
+		    return self.cues[self.currentCue]["interps"]
         return None
 
     def setGlobalInterpTime(self, value, allcues):
