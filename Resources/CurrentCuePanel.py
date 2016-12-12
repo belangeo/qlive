2e840673a769e66fc8a30e13b11c87007313dda8
Olivier Belanger <belangeo@gmail.com>
2016-12-12 15:12:12 -0500

    Fixed Crash on quit if currentCueView was closed manually.

--- Resources/CurrentCuePanel.py
+++ Resources/CurrentCuePanel.py
@@ -1,19 +1,23 @@
 # encoding: utf-8
 import wx
 import QLiveLib
+from constants import *
 
 class CurrentCuePanel(wx.Frame):
     def __init__(self, parent):
-        style = wx.DEFAULT_FRAME_STYLE & (~wx.CLOSE_BOX) & (~wx.MINIMIZE_BOX)
-        wx.Frame.__init__(self, parent, title="Current Cue Window", style=style, size=(600, 600))
+        wx.Frame.__init__(self, parent, title="Current Cue Window", 
+                          size=(600, 600))
+        self.parent = parent
         self.SetBackgroundColour(wx.BLACK)
+        self.Bind(wx.EVT_CLOSE, self.close)
 
         # TODO: set text during the obj creation
         # TODO: auto adjust font size to the window size
         str = "0"
         self.text = wx.StaticText(self, -1, str)
         self.text.SetForegroundColour((255,255,255))
-        font = wx.Font(450, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, wx.FONTWEIGHT_BOLD)
+        font = wx.Font(450, wx.FONTFAMILY_DEFAULT, wx.FONTSTYLE_NORMAL, 
+                       wx.FONTWEIGHT_BOLD)
         self.text.SetFont(font)
 
         # Vertical/Horizontal centering
@@ -22,6 +26,12 @@ class CurrentCuePanel(wx.Frame):
         sizer_h.Add(self.text, 1, wx.CENTER)
         sizer_v.Add(sizer_h, 1, wx.CENTER)
         self.SetSizer(sizer_v)
+
+    def close(self, evt):
+        menubar = self.parent.GetMenuBar()
+        item = menubar.FindItemById(VIEW_CUE_WINDOW_ID)
+        item.Check(False)
+        self.Hide()
 
     def refresh(self, cue):
         wx.CallAfter(self.text.SetLabel, str(cue))
